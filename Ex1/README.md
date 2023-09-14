0b353d30185d9b85afc99f2682fd32640cc08773
dallas145 <mikelg512@gmail.com>
2023-09-14 13:55:45 +0800

    ex1 finish

---
+++ Ex1/README.md
@@ -0,0 +1,22 @@
+# 費氏數列遞迴版
+
+## 問題：
+* 在Powrshell執行時，大多無法顯示正確程式耗時，須在WSL環境下才能正確顯示
+
+## 執行結果
+
+* Powershell: Python 3.9.16
+```
+(base) PS C:\Users\mikel\Github\alg112a\Ex1> python fib_loop.py
+fibonacci(60) = 1548008755920
+time: 0.000000
+time:0:00:00
+```
+
+* WSL: Python 3.10.6
+```
+neil@B660M12400:/mnt/c/Users/mikel/Github/alg112a/Ex1$ python3 fib_loop.py
+fibonacci(60) = 1548008755920
+time: 0.000047
+time:0:00:00.000046
+```
---
+++ Ex1/fib_loop.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import time
+
+def fibonacci(n):
+    fib = [1, 1]
+    for i in range(n-2):
+        _next = fib[i] + fib[i+1]
+        fib.append(_next)
+
+    return fib[len(fib)-1]
+
+# n = 35
+n = 60
+sT = time.time() # 使用time module
+startTime = datetime.now() # 使用datetime module
+print("fibonacci(%d) = %d" % (n, fibonacci(n)))
+eT = time.time() # 使用time module
+endTime = datetime.now() # 使用datetime module
+sec = eT - sT # 使用time module
+seconds = endTime - startTime # 使用datetime module
+print("time: %f"%(sec)) # 使用time module
+print("time:{}".format(seconds)) # 使用datetime module
